4894e6abd85ddb8cbf3401e2b586e484372d204c
icecrimu <charles.banigon@gmail.com>
2022-04-13 12:11:20 +0800

    Add OperationButton component

---
+++ src/components/OperationButton.js
@@ -0,0 +1,13 @@
+import { ACTIONS } from "../App"
+
+export default function OperationButton({ operation, dispatch }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  )
+}
